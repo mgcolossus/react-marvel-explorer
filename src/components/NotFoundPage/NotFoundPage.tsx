975c403360c6d8bd0367db0ee2766fba6f816534
Magomed <colossus130600@yandex.ru>
2021-06-20 15:32:03 +0300

    Add NoFoundPage component

---
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const NotFoundPage = () => (
+  <div className="not-found-page-block">
+    <div className="not-found-page-block__text">No page found</div>
+  </div>
+);
