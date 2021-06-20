35cc3663c14e952177bebde8d93545a5f908ebf4
Magomed <colossus130600@yandex.ru>
2021-06-20 14:36:28 +0300

    Add SectionLoading component and its styles and inner image

---
+++ src/components/shared/SectionLoading/SectionLoading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoadingSvg from "./SectionLoading.svg";
+
+export const SectionLoading = () => (
+  <div className="details-section__loading">
+    <img src={LoadingSvg} alt="loading" />
+  </div>
+);
