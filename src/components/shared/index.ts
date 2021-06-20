c1927e731a2d1d296fcd84321a22ac09fbebed42
Magomed <colossus130600@yandex.ru>
2021-06-20 14:37:42 +0300

    Add index file for 'shared' folder

---
+++ src/components/shared/index.ts
@@ -0,0 +1,16 @@
+import { PageCardLink } from "./PageCardLink/PageCardLink";
+import { LoadingLogo } from "./LoadingLogo/LoadingLogo";
+import { SectionLoading } from "./SectionLoading/SectionLoading";
+import { ErrorBlock } from "./ErrorBlock/ErrorBlock";
+import { Pagination } from "./Pagination/Pagination";
+import { Page } from "./Page/Page";
+import { ComicsSection } from "./DetailsSections/ComicsSection";
+import { EventsSection } from "./DetailsSections/EventsSection";
+import { SeriesSection } from "./DetailsSections/SeriesSection";
+import { StoriesSection } from "./DetailsSections/StoriesSection";
+import { CharactersSection } from "./DetailsSections/CharactersSection";
+import { CreatorsSection } from "./DetailsSections/CreatorsSection";
+
+export {
+  PageCardLink, LoadingLogo, SectionLoading, ErrorBlock, Pagination, Page, ComicsSection, EventsSection, SeriesSection, StoriesSection, CharactersSection, CreatorsSection
+};
